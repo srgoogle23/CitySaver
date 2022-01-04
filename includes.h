da5fc71b66e09bb5dbc82112dd51af6523e7f215
Leonardo Oliveira <62403037+srgoogle23@users.noreply.github.com>
2022-01-04 12:02:15 -0300

    feat: :ambulance: iniciado atribuição de valores e abstração do game

--- includes.h
+++ includes.h
@@ -1,2 +1,10 @@
 // Bibliotecas necessárias do Allegro
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+
+// Bibliotecas necessárias Nativas
+#include <stdio.h>
--- main.c
+++ main.c
@@ -1,2 +1,33 @@
 // Arquivo separado com todos os includes necessários para a execução do programa
 #include "includes.h"
+const int largura_game = 800;
+const int altura_game = 600;
+const int sair_game = 1;
+
+struct Game
+{
+    ALLEGRO_BITMAP* fundo;
+    ALLEGRO_DISPLAY *janela;
+    ALLEGRO_FONT *fonte;
+    ALLEGRO_EVENT_QUEUE *filaEventos;
+    ALLEGRO_TIMER *timer;
+    int largura;
+    int altura;
+    int sair;
+};
+
+int main()
+{
+    // iniciando váriavel do game
+    struct Game game;
+    
+    // atribuindo valores aos atributos da struct game
+    game.largura    = largura_game;
+    game.altura     = altura_game;
+    game.sair       = sair_game;
+
+    printf("%d\n", game.largura);
+    printf("%d\n", game.altura);
+    printf("%d\n", game.sair);
+
+}
