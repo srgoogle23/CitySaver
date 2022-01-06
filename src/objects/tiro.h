c507913d26573c0208f54d596ee455cab0c1b58a
Leonardo Oliveira <62403037+srgoogle23@users.noreply.github.com>
2022-01-06 15:35:50 -0300

    feat: :ambulance: limpa fila de tiros

--- src/objects/tiro.h
+++ src/objects/tiro.h
@@ -4,6 +4,9 @@ void teclasTiro(int tecla, int tipoEvento);
 void calculaMovimentoTiro();
 void redesenhaTiro();
 void mudaAnimacaoTiro(int i);
+void verificaSeOTiroEstaNaTela(int i);
+int verificaSeTodosOsTirosEstaoDesativados();
+void limpaTiros();
 
 struct Tiro {
     bool ativado;
@@ -55,12 +58,22 @@ void disparaTiro()
 
         al_draw_bitmap_region(tiros, struct_tiro[filaTiros-1].offset_x, 0, struct_tiro[filaTiros-1].largura, struct_tiro[filaTiros-1].altura, struct_tiro[filaTiros-1].x, struct_tiro[filaTiros-1].y, 0);
     }
+    else
+    {
+        if(verificaSeTodosOsTirosEstaoDesativados())
+        {
+            limpaTiros();
+        }
+    }
+
+    printf("filaTiros: %d\n", filaTiros);
 }
 
 void calculaMovimentoTiro()
 {
     for (int i = 0; i < quantidadeTiros; i++)
     {
+        verificaSeOTiroEstaNaTela(i);
         if (struct_tiro[i].ativado == true)
         {
             mudaAnimacaoTiro(i);
@@ -69,6 +82,44 @@ void calculaMovimentoTiro()
             struct_tiro[i].altura = tiros_tamanho[struct_tiro[i].tipo][1];
             struct_tiro[i].offset_x = tiros_tamanho[struct_tiro[i].tipo][2];
         }
+    }
+}
+
+void limpaTiros()
+{
+    for (int i = 0; i < quantidadeTiros; i++)
+    {
+        struct_tiro[i].ativado = false;
+        struct_tiro[i].tipo = 0;
+        struct_tiro[i].x = 0;
+        struct_tiro[i].y = 0;
+        struct_tiro[i].x_inicial = 0;
+        struct_tiro[i].y_inicial = 0;
+        struct_tiro[i].largura = 0;
+        struct_tiro[i].altura = 0;
+        struct_tiro[i].offset_x = 0;
+        struct_tiro[i].velocidade = velocidadeTiroSimples;
+    }
+    filaTiros = 0;
+}
+
+int verificaSeTodosOsTirosEstaoDesativados()
+{
+    for (int i = 0; i < quantidadeTiros; i++)
+    {
+        if (struct_tiro[i].ativado == true)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void verificaSeOTiroEstaNaTela(int i)
+{
+    if(struct_tiro[i].x > SCREEN_W)
+    {
+        struct_tiro[i].ativado = false;
     }
 }
 
